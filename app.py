2ee5d2883248cfa5d002dcafb2650757e3c10aea
siIverfish <103962839+siIverfish@users.noreply.github.com>
2024-01-25 20:45:47 +0000

    iter changes

--- app.py
+++ app.py
@@ -16,20 +16,32 @@ import rospy
 from collections import deque
 from typing import List
 
-# todo: move these into their respective classes
-TEAM_NUMBER = "3952"
-CLIENT_NAME = "3952-odometry-subscriber"
 TABLE_NAME = "datatable"
-TOPIC_NAME  = "estimatedOdometryPosition"
 NT_INSTANCE = ntcore.NetworkTableInstance.getDefault()
 NT_TABLE = NT_INSTANCE.getTable(TABLE_NAME)
-PUBLISH_TOPIC_NAME = "finalRobotPosition"
+
+
+def main():
+    odometry_position_subscriber = OdometryPositionSubscriber()
+    apriltag_position_subscriber = ApriltagPositionSubscriber()
+    localizer = Localizer()
+    new_position_publisher = NewPositionPublisher()
+
+    localization_inputs = zip(odometry_position_subscriber, apriltag_position_subscriber)
+    final_positions = map(localizer, localization_inputs)
+    new_position_publisher.publish_each(final_positions)
+
 
 class OdometryPositionSubscriber:
-    def __init__(self):
-        self.nt_position_subscriber = NT_TABLE.getDoubleArrayTopic(TOPIC_NAME).subscribe(0)
-        self.nt_instance.startClient4(CLIENT_NAME) # i do not know what this does
-        self.nt_instance.setServerTeam(TEAM_NUMBER) # where TEAM=190, 294, etc, or use inst.setServer("hostname") or similar
+    NT_ODOMETRY_TOPIC_NAME  = "estimatedOdometryPosition"
+    CLIENT_NAME = "3952-odometry-subscriber"
+    TEAM_NUMBER = "3952"
+
+    def __init__(self, *, table):
+        self.nt_position_subscriber = NT_TABLE.getDoubleArrayTopic(self.NT_ODOMETRY_TOPIC_NAME).subscribe(0)
+        # these values are probably mostly diagnostic
+        self.nt_instance.startClient4(self.CLIENT_NAME) # i do not know what this does
+        self.nt_instance.setServerTeam(self.TEAM_NUMBER) # where TEAM=190, 294, etc, or use inst.setServer("hostname") or similar
 
     async def __iter__(self):
         # todo: make the `time.sleep` call here start counting the 1 second before the yield
@@ -57,21 +69,27 @@ class ApriltagPositionSubscriber:
 
 
     def __init__(self, *, callback_function=None):
-        # FILO queue for iteration
-        self.queue = deque()
+        # Optional most recent value, returned by the next() method.
+        # if the next() method is called while this is None,
+        # it waits until it receives a new value from isaac_ros to return.
+        self.value = None
 
         rospy.init_node(self.NODE_NAME)
         rospy.Subscriber(self.ROS_TOPIC_NAME, self.DATA_TYPE, callback_function)
     
     
     async def callback(self, tag_detections):
-        self.queue.appendleft(tag_detections)
+        self.value = tag_detections
     
     
     async def __iter__(self):
         while True:
-            if self.queue:
-                yield self.queue.pop()
+            if self.value:
+                # this could be better, find an Optional<> datatype
+                # like Rust has & import it or something.
+                temp = self.value
+                self.value = None
+                yield temp
             time.sleep(0.1)
 
 
@@ -84,24 +102,16 @@ class Localizer:
 
 
 class NewPositionPublisher:
-    # this class may be unnecessary
-    def __init__(self):
-        self._publisher = NT_TABLE.getDoubleArrayTopic(PUBLISH_TOPIC_NAME).publish()
+    PUBLISH_TOPIC_NAME = "finalRobotPosition"
+
+    def __init__(self, *, table):
+        self._publisher = table\
+            .getDoubleArrayTopic(self.PUBLISH_TOPIC_NAME)\
+            .publish()
     
     def publish_each(self, values):
         for value in values:
             self._publisher.set(values)
-
-
-def main():
-    odometry_position_subscriber = OdometryPositionSubscriber()
-    apriltag_position_subscriber = ApriltagPositionSubscriber()
-    localizer = Localizer()
-    new_position_publisher = NewPositionPublisher()
-
-    localization_inputs = zip(odometry_position_subscriber, apriltag_position_subscriber)
-    final_positions = map(localizer, localization_inputs)
-    new_position_publisher.publish_each(final_positions)
 
 
 if __name__ == "__main__":
