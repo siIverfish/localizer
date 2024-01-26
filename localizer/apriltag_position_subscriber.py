a1440400ccaf8d451ff3fe05da8206e7213bca20
siIverfish <103962839+siIverfish@users.noreply.github.com>
2024-01-25 18:40:14 -0800

    add tedtalk to apriltags subscriber

--- localizer/apriltag_position_subscriber.py
+++ localizer/apriltag_position_subscriber.py
@@ -2,9 +2,18 @@
 This class works similarly to the odometrey subscriber, but with different way of retrieving values. 
 (See odometry_position_subscriber for an explanation of the iterator situation). 
 
-To get a value, it has to hook into rclpy and run whatever subscriber code they have over there. Then, it checks if the hook caught any fish (if anything called the callback function) and only then does it get anything to return.
+To get a value, it has to hook into rclpy and run whatever subscriber code they have over there. 
+Then, it checks if the hook caught any fish (if anything called the callback function) and only then does it get anything to return.
 
-May have to alter this file to spin until it gets a new value.
+May have to alter this file to spin until it gets a new value. Spinning pile up past work and eventually be way backed up.
+
+The problem with this code is that rclpy *really* wants to be a whole framework that encompasses all of our code, but it can't be --  we need other inputs too. 
+
+This led me to think "wow! this code would turn out to be so easy if we did just that -- made a callback function that grabs odometry outputs itself and publishes them to network tables right there in the method." 
+Then I realized I already *had* written code that did that, I was just dumb and didn't realize how my own iterators worked.
+The `yield` statement in this function already triggers all of the rest of the iterator path, sending it down through the chain until eventually it is published.
+
+thanks for listening to my tedtalk - ivan
 """
 
 import rclpy
