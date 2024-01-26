a417398a39ac3d9333b895aab31b606aba987980
siIverfish <103962839+siIverfish@users.noreply.github.com>
2024-01-25 18:16:24 -0800

    odometrey comments explaining cursed iterator situation

--- localizer/odometry_position_subscriber.py
+++ localizer/odometry_position_subscriber.py
@@ -1,3 +1,15 @@
+"""
+This class (and the AprilTagPositionSubscriber)  *seem* simple, but actually do not work very well as traditional iterators. 
+The assumption about this iterator is that every time the robot publishes a value, it is collected in a queue and eventually fed out via the next method.
+However, this class doesn't have a queue: rather, there is a single `last_result` value updated whenever the loop runs. 
+If you read this as normal, non-generator code, it seems like it would continuously update and output values.
+But it is not. Because of the gap after each yield statement as the localizer runs, it will *skip multiple values* that are never retrieved. In fact, when the next method is called, it will:
+    a. return the value it gets polling network tables because it recognizes that it's a new value.
+    b. poll and get the same value as last time (should be rarer, might never happen) in which case it'll re-poll until it gets something new.
+
+Basically, the point is that the next() method will *always* return the most recent value. This circumvents problems with the iterators getting out of sync as one goes faster than the other.
+"""
+
 class OdometryPositionSubscriber:
     NT_ODOMETRY_TOPIC_NAME  = "estimatedOdometryPosition"
     CLIENT_NAME = "3952-odometry-subscriber"
