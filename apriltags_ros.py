d3cf9975fdef8fdeca33c8a1f4c16121209f07e8
siIverfish <103962839+siIverfish@users.noreply.github.com>
2024-01-30 00:14:49 +0000

    aa

--- apriltags_ros.py
+++ apriltags_ros.py
@@ -4,3 +4,4 @@ class AprilTagDetectionArray(genpy.Message):
   _md5sum = "93c0a301ed9e6633dc34b8117d49ebd4"
   _type = "apriltags_ros/AprilTagDetectionArray"
   _has_header = True #flag to mark the presence of a Header object
+  _TYPE_SUPPORT = True
