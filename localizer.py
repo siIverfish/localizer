3962b704ccd6449c761eb66a1994bd7d09800fc6
siIverfish <103962839+siIverfish@users.noreply.github.com>
2024-02-06 23:51:57 +0000

    concatenate apriltag positions

--- localizer.py
+++ localizer.py
@@ -1,7 +1,15 @@
 from typing import List
 from isaac_ros_apriltag_interfaces.msg import AprilTagDetectionArray
 
+from itertools import chain
+
+# no longer localizes. now just extracts april tag positions.
+# rip bad name, will eventually fix
+
 class Localizer:
     def __call__(self, april_tag_detection_array):
-        pos = april_tag_detection_array.detections[0].pose.pose.pose.position
-        return [pos.x, pos.y, pos.z]
+        concatenated_positions = []
+        for detection in april_tag_detection_array.detections:
+            position = detection.pose.pose.pose.position
+            concatenated_positions.extend((position.x, position.y, position.z))
+        return concatenated_positions
